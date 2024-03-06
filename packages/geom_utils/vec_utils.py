f7e12d7ad74154370f6e3e8abaa53d10dc548ae1
boblyx <bobyxlee@gmail.com>
2024-03-06 11:38:32 +0800

    Completed synthetic data creation. Progress of GNN

--- packages/geom_utils/vec_utils.py
+++ packages/geom_utils/vec_utils.py
@@ -1,7 +1,14 @@
+from copy import deepcopy
 import math
 import numpy as np
 
 def vec2dAngle(v1, v2):
+    """
+    :param v1: Vector 1
+    :param v2: Vector 2
+    :return: Angle between the two vectors.
+
+    """
     return math.atan2(
             v1[1] * v2[0] - v1[0] * v2[1],
             v1[0] * v2[0] + v1[1] * v2[1]
@@ -12,5 +19,27 @@ def pointsTo2DVec(s, t):
     :param s: Source
     :param t: Target
     :return: Vector as a 2D array
+
     """
     return([t[0] - s[0], t[1] - s[1]])
+
+def rotatePointByPivot(pivot = [0, 0], target = [0, 1], angle = math.pi/2):
+    """
+    :param pivot: Pivot around which point is to be rotated
+    :param target: Target point to rotate
+    :param angle: Rotatation angle in radians.
+    :return: New point that is rotated.
+
+    """
+
+    sa = math.sin(angle)
+    ca = math.cos(angle)
+
+    tc = deepcopy(target)
+    tc[0] -= pivot[0]
+    tc[1] -= pivot[1]
+
+    x = tc[0] * ca - tc[1] * sa
+    y = tc[0] * sa + tc[1] * ca
+
+    return [x + pivot[0], y + pivot [1]]
--- packages/synth/synthesise.py
+++ packages/synth/synthesise.py
@@ -9,28 +9,20 @@ import math
 from copy import deepcopy
 from uuid import uuid4
 from random import randrange, uniform
-sys.path.append('./packages')
+sys.path.append('../')
 from geom_utils import vec_utils #.vec_utils import vec2dAngle, pointsTo2DVec
 
 BASE = {"e": [], "p": [], "s": []}
+N = 50
 
 def rNum():
-    return randrange(-300,300)
+    return randrange(-N,N)
 
 def rRad():
     """
     Generates random radians between pi/8 to pi/2
     """
     return uniform(math.pi/8, math.pi/2)
-
-def rotate(shape_verts, angle = 0):
-    """
-    Rotates a sequence of vertices by given angle in radians.
-    :param shape_verts: List of shape vertices.
-    :param angle: Angle to rotate in radians.
-    """
-
-    pass
 
 def gPoint():
     """
@@ -68,14 +60,30 @@ def gTri():
     v2 = [0, 1]
     r = rRad() + vec_utils.vec2dAngle(v1,v2)
     p3 = vec_utils.rotatePointByPivot(l[0], l[1], r)
-    print([l[0], l[1], p3])
+
+    d = deepcopy(BASE)
+    d["e"] = [[0,1],[1,2],[2,0]]
+    d["p"] = [l[0], l[1], p3]
+    d["s"] = [2]
+    return d
 
 def gSquare():
     """
     Generates a square representation of random orientation.
-
+    - Generate a line first (point A, B)
+    - Rotate startpoint by 270 deg using end as pivot(point D)
+    - Rotate endpoint by 90 deg using start as pivot(point C) 
     """
-    pass
+    A = [rNum(), rNum()]
+    B = [rNum(), rNum()]
+    C = vec_utils.rotatePointByPivot(A, B, math.pi/2)
+    D = vec_utils.rotatePointByPivot(B, A, 3*math.pi/2)
+
+    d = deepcopy(BASE)
+    d["e"] = [[0,1],[1,2],[2,3],[3,0]] # May jumble up start index,but similar order
+    d["p"] = [A, B, C, D]
+    d["s"] = [3]
+    return d
 
 if __name__ == "__main__":
     gTri();
